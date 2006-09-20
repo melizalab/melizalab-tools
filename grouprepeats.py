65912855d69f8ca0387aee47c0453b5265d83a22
dmeliza <dmeliza@uchicago.edu>
2006-09-20 17:11:57 -0500

    modularized grouprepeats
    Split grouprepeats() into two functions so that the inner one, which
    ignores basename, can be called directly in the case when the user
    wants to collect repeats across basenames. Also, now all the file
    operations are in the __main__ section so the functions will actually
    be useful as module components
    
    darcs-hash:20060920221157-21d25-b61ad9fdd79cd367312341c9724b50fac2c0cb41.gz

--- grouprepeats.py
+++ grouprepeats.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 """
-grouprepeats.py - a function for grouping the event information in toe_lis
+grouprepeats.py - a module for grouping the event information in toe_lis
                   files into multi-repeat toe_lis files, grouped by
                   any number of parameters
 """
 
 import episode, toelis
 
-def grouprepeats(explog, basename_list=None, stimulus_list=None):
+def grouprepeats(episodes, basename_list=None, stimulus_list=None):
     """
-    Group repeats by stimulus. Collects all the toe_lis files associated
-    with a particular stimulus and generates multi-repeat toe_lis files
-    named by the stimulus.  For instance, if the basename was 'zf_bl72a',
-    the output files would be 'zf_bl72a_stim1.toe_lis', etc.  By default,
-    output is grouped by basename, and all the stimuli and basenames are
-    processed. This behavior can be altered by specifying a list of
-    basenames or a list of stimuli, in which case only those basenames
-    and stimuli will be processed.
-    """
-    # wrap the stimulus_list if needed
-    if stimulus_list:
-        stimulus_list = isinstance(stimulus_list,str) and set([stimulus_list]) or set(stimulus_list)
+    
+    Group repeats by stimulus and basename. Collects all the toe_lis
+    files associated with a particular stimulus and generates
+    multi-repeat toe_lis files named by the stimulus.  For instance,
+    if the basename was 'zf_bl72a', the output files would be
+    'zf_bl72a_stim1.toe_lis', etc.  By default, output is grouped by
+    basename, and all the stimuli and basenames are processed. This
+    behavior can be altered by specifying a list of basenames or a
+    list of stimuli, in which case only those basenames and stimuli
+    will be processed. Use groupstimuli to ignore the basename
+    dimension.
 
-    # read in episodes
-    episodes = episode.readexplog(explog)
+    Returns a dictionary of dictionaries, keyed by basename and then
+    stimulus.
+    """
+
 
     # need unique basenames, since we do this analysis on a per-site basis
     basenames = set([ep.basename for ep in episodes])
@@ -32,52 +33,72 @@ def grouprepeats(explog, basename_list=None, stimulus_list=None):
         basenames = basenames & \
                     (isinstance(basename_list,str) and set([basename_list]) or set(basename_list))
 
+    tl_dict = {}
     for basename in basenames:
         print "Processing %s: " % basename
         bn_episodes = [ep for ep in episodes if ep.basename==basename]
 
-        # unique stimuli
-        def add(x,y): return x+y
-        stimuli  = reduce(add, [ep.stimulus for ep in bn_episodes])
-        stimuli  = set(stimuli)
-        if stimulus_list:
-            stimuli = stimuli & stimulus_list
 
-        for stimulus in stimuli:
-            bn_st_episodes = [ep for ep in bn_episodes if stimulus in ep.stimulus]
+        tl_dict[basename] = groupstimuli(bn_episodes, stimulus_list)
 
-            # load all the toelis files, merge them, and write a new toelis
-            if len(bn_st_episodes)==0:
-                # this indicates a programming error or something serious
-                print "stim %s has no matches!" % stimulus
-                continue
-            
-            toelises = None
-            entries = []
-            for ep in bn_st_episodes:
-                entries.append(ep.entry)
-                toelis_name = "%s_%03d.toe_lis" % (ep.basename, ep.entry)
-                try:
-                    tl = toelis.readfile(toelis_name)
-                    # align episodes to the start of the first stimulus (usually the only one)
-                    tl.offset(-ep.stim_start[0])
-                    if toelises:
-                        toelises.extend(tl)
-                    else:
-                        toelises = tl
-                except:
-                    print "** Error processing file %s, skipping" % toelis_name
-
-            print "stim %s matches %s" % (stimulus, entries)
-            if toelises:
-                toelis_name = "%s_%s.toe_lis" % (basename, stimulus)
-                toelis.writefile(toelis_name, toelises)
-                print "Wrote combined repeats to %s" % toelis_name
-            else:
-                print "No (valid) matches for stimulus %s in this entry " % stimulus
-
-    print "Done!"
+    return tl_dict
 # end grouprepeats
+
+
+def groupstimuli(episodes, stimulus_list=None):
+    """
+    This function groups all the toelis data for stimuli in the supplied
+    episodes argument, ignoring basenames. By default, all stimuli will be
+    used; limit to particular stimuli by supplying the stimulus_list argument.
+
+    Returns a dictionary of toelis objects, keyed by stimulus name
+    """
+    # wrap the stimulus_list if needed
+    if stimulus_list:
+        stimulus_list = isinstance(stimulus_list,str) and set([stimulus_list]) or set(stimulus_list)
+
+    # determine unique stimuli
+    def add(x,y): return x+y
+    stimuli  = reduce(add, [ep.stimulus for ep in episodes])
+    stimuli  = set(stimuli)
+    if stimulus_list:
+        stimuli = stimuli & stimulus_list
+
+    tl_dict = {}
+    for stimulus in stimuli:
+        st_episodes = [ep for ep in episodes if stimulus in ep.stimulus]
+
+        # load all the toelis files, merge them, and write a new toelis
+        if len(st_episodes)==0:
+            # this indicates a programming error or something serious
+            # but in the interests of robustness, it just gets skipped
+            print "stim %s has no matches!" % stimulus
+            continue
+            
+        toelises = None
+        entries = []
+        for ep in st_episodes:
+            entries.append(ep.entry)
+            toelis_name = "%s_%03d.toe_lis" % (ep.basename, ep.entry)
+            try:
+                tl = toelis.readfile(toelis_name)
+                # align episodes to the start of the first stimulus (usually the only one)
+                tl.offset(-ep.stim_start[0])
+                if toelises:
+                    toelises.extend(tl)
+                else:
+                    toelises = tl
+            except:
+                print "** Error processing file %s, skipping" % toelis_name
+
+            tl_dict[stimulus] = toelises
+        print "stim %s matches %s" % (stimulus, entries)
+
+
+    return tl_dict
+# end groupstimuli
+
+
 
 
 if __name__=="__main__":
@@ -85,20 +106,26 @@ if __name__=="__main__":
     import sys, getopt
 
     def usage():
-        print "Usage: grouprepeats.py [--basename={...}] [--stimulus={...}]<explog>\n"
-        print "       basename specifies which files to include in the grouping."
+        print "Usage: grouprepeats.py [--file=\"...\"] [--stimulus=\"...\"] "
+        print "                       [--allfiles [--basename=<basename>]] <explog>\n"
+        print "       --file specifies which files to include in the grouping."
         print "       Supply a comma-delimited list of basenames to include, e.g."
-        print "       --basename={st302_20060904b, st302_20060904c}\n"
-        print "       stimulus specifies which stimuli to include in the grouping."
+        print "       --file='st302_20060904b, st302_20060904c'\n"
+        print "       --stimulus specifies which stimuli to include in the grouping."
         print "       Supply a comma-delimited list of the stimuli, without extensions, e.g."
-        print "       --stimulus={A,B,C}\n"
-        print "       Note that output is ALWAYS grouped by basename/stimulus. The"
-        print "       output files have the name '<basename>_<stimulus>.toe_lis"
+        print "       --stimulus='A,B,C' (single or double quotes required)\n"
+        print "       The --allfiles flag instructs grouprepeats to group only by stimulus."
+        print "       By default, output is grouped by basename and stimulus, and the "
+        print "       output files have the name '<basename>_<stimulus>.toe_lis'; if this"
+        print "       option is set, output files have the name '<stimulus>.toe_lis'\n"
+        print "       --basename specifies a replacement base file name; only applies"
+        print "       if --allfiles is set. Output will be <basename>_<stimulus>.toe_lis\n"
         sys.exit(-1)
         
     if len(sys.argv) < 2: usage()
     
-    opts, args = getopt.getopt(sys.argv[1:], "h", ["basename=", "stimulus=", "help"])
+    opts, args = getopt.getopt(sys.argv[1:], "h", \
+                               ["basename=", "stimulus=", "help", "allfiles", "file="])
 
     opts = dict(opts)
     if opts.get('-h') or opts.get('--help'): usage()
@@ -106,15 +133,50 @@ if __name__=="__main__":
         print "Error: need an explog"
         usage()
 
-    basename_list = None
+    def trim(x): return x.strip()
+    
+    file_list = None
     stimulus_list = None
+    custom_basename = None
+    allfiles = False
     for o,a in opts.items():
-        if o == '--basename':
-            basename_list = a[1:-1].split(',')
+        if o == '--file':
+            file_list = map(trim, a.split(','))
         elif o == '--stimulus':
-            stimulus_list = a[1:-1].split(',')
+            stimulus_list = map(trim, a.split(','))
+        elif o == '--allfiles':
+            allfiles = True
+        elif o == '--basename':
+            custom_basename = a
 
-    grouprepeats(args[0], basename_list, stimulus_list)
+    # read in episodes
+    episodes = episode.readexplog(args[0])
+
+    if not allfiles:
+        tl_dict = grouprepeats(episodes, file_list, stimulus_list)
+        for (basename, obj) in tl_dict.items():
+            for (stimname, tl) in obj.items():
+                if tl:
+                    toelis_name = "%s_%s.toe_lis" % (basename, stimname)
+                    tl.writefile(toelis_name)
+                    print "Wrote combined repeats to %s" % toelis_name
+    else:
+        # filter episodes on file_list here
+        if file_list:
+            file_list = (isinstance(file_list,str) and set([file_list]) or set(file_list))
+            episodes = [ep for ep in episodes if ep.basename in file_list]
+        tl_dict = groupstimuli(episodes, stimulus_list)
+        for (stimname, tl) in tl_dict.items():
+            if tl:
+                if custom_basename:
+                    toelis_name = "%s_%s.toe_lis" % (custom_basename, stimname)
+                else:
+                    toelis_name = "%s.toe_lis" % stimname
+                    
+                tl.writefile(toelis_name)
+                print "Wrote combined repeats to %s" % toelis_name
+    print "Done!"
+                
         
 
             
