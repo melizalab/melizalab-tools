2f71ab2e42ce9afb9524df53e325f68de2fec3ac
dmeliza <dmeliza@uchicago.edu>
2006-12-18 17:20:26 -0600

    added scripting to tplot
    darcs-hash:20061218232026-21d25-a8882807ed37b696084d243876c8492b591bd367.gz

--- tplot.py
+++ tplot.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 """
-tplot.py <file>
+tplot.py [flags] <files>
 
     tplot is a general purpose time series plotting tool. It can plot
     toe_lis data as rasters or histograms, and pcm data as oscillograms
-    or spectrograms
+    or spectrograms. It accepts a list of files, which will be each be plotted
+    in separate plots arranged on a vertical axis.
 
   CDM, 12/2006
  
@@ -20,7 +21,7 @@ def plotraster(events, g=None, unit=0, pointtype=7, pointsize=0.8, pointcolor=0)
     """
 
     if not g:
-        g = Gnuplot(persist=1)
+        g = Gnuplot()
 
     nreps = events.nrepeats
     (x,y) = events.rasterpoints(unit)
@@ -35,7 +36,7 @@ def plothist(events, g=None, unit=0, binsize=20, normalize=False):
     Plots event data as a histogram
     """
     if not g:
-        g = Gnuplot(persist=1)
+        g = Gnuplot()
 
     (bins, freq) = events.histogram(unit=unit, binsize=binsize, normalize=normalize)
     g('set boxwidth 1.0 relative')
@@ -44,3 +45,69 @@ def plothist(events, g=None, unit=0, binsize=20, normalize=False):
     g.plot(d)
     return g
 
+def plotpcmfile(filename, g=None, format="%int16", linecolor=0):
+    """
+    Plots pcm data from a file. Gnuplot can read binary files directly if
+    given file format information. PCM data is 16-bit integer, little-endian
+    """
+    if not g:
+        g = Gnuplot()
+
+    with="l lt 1 lc %d" % linecolor
+    g("plot '%s' binary format='%s' using 1 with %s title ''" % (filename, format, with))
+    return g
+
+def plotts(data, g=None, linetype=1, linecolor=0):
+    """
+    Plots a single column of data as a line. Useful for plotting pcm data
+    """
+    if not g:
+        g = Gnuplot()
+
+    with = "l lt %d lc %d" % (linetype, linecolor)
+    g.plot(Data(data, with=with))
+
+
+
+if __name__=="__main__":
+
+    import sys, os
+
+    toefun = plotraster
+    tsfun = plotpcmfile
+
+    # first load the data
+    plotdata = []
+    for arg in sys.argv[1:]:
+
+        if arg == '-r':
+            toefun = plotraster
+        elif arg == '-h':
+            toefun = plothist
+        elif arg == '-o':
+            tsfun = plotpcmfile
+
+        else:
+            (base, ext) = os.path.splitext(arg)
+
+            if ext == '.toe_lis':
+                plotdata.append((toelis.readfile(arg), toefun, base))
+            elif ext == '.pcm':
+                plotdata.append((arg, tsfun, base))
+
+    # now plot it
+    nplots = len(plotdata)
+    g = Gnuplot(persist=1)
+    g("set multiplot layout %d,1" % nplots)
+    g("set format x ''")
+    for i in range(nplots):
+        (data, plotfun, title) = plotdata[i]
+        if i == (nplots - 1):
+            g("set format x")
+            g.xlabel("Time (ms)")
+
+        #g.title(title, offset=(0,-1))
+        g("set y2label '%s' " % title)
+        g("set lmargin 7")
+        g = plotfun(data, g)
+    
