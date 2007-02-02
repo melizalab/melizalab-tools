60184e4f0c808b04ffd2d428908efe1af60b826e
dmeliza <dmeliza@uchicago.edu>
2007-02-02 13:23:06 -0600

    klusters now has a convenient all in one group extraction method
    darcs-hash:20070202192306-21d25-4981235504f06d4d2ea22de205ea8dff614b9ad2.gz

--- spikes/klusters.py
+++ spikes/klusters.py
@@ -7,7 +7,6 @@ Processes pcm_seq2 data for use by klusters
 from extractor import *
 import tables as t
 from dlab import explog
-import pdb
 
 class filecache(dict):
     """
@@ -29,21 +28,6 @@ class filecache(dict):
     def __setitem__(self, key, value):
         raise NotImplementedError, "Use getter methods to add items to the cache"
 
-##     def __calcstats(self, key):
-##         stats = signalstats(self.get(key))
-##         self.rms[key] = stats['rms']
-##         self.dcoff[key] = stats['dcoff']
-
-##     def rms(self, key):
-##         if not self.rms.has_key(key):
-##             self.__calcstats(key)
-##         return self.rms[key]
-
-##     def dcoff(self, key):
-##         if not self.dcoff.has_key(key):
-##             self.__calcstats(key)
-##         return self.dcoff.get(key)
-
 class statscache(filecache):
 
     def _handler(self, filename):
@@ -52,73 +36,102 @@ class statscache(filecache):
     def __init__(self, filecache):
         self._filecache = filecache
 
-class site(object):
+
+class site(explog.explog):
     """
     The site class represents a recording site. For each electrode,
     saber will generate a separate pcm_seq2 file.  In each file there
     will be entries for each episode of recording.
     """
 
-    def __init__(self, explog, pen, site):
+    def __init__(self, logfile, pen, site):
         """
         Initialize the object using an explog and specifying
         a recording site.
         """
-
-        if explog.endswith('.h5'):
-            self.elog = t.openFile(explog, 'r')
-        else:
-            self.elog = explog.readexplog(explog, explog + '.h5')
+        explog.explog.__init__(self, logfile)
 
         self.site = (pen,site)
-        self.__getchannels()
         self._filecache = filecache()
         self._statscache = statscache(self._filecache)
-        self._timestamps = self.getabstimes()
+
 
     def __del__(self):
         self.elog.close()
 
     def getsiteentry(self, entry, channels=None):
+        return explog.explog.getsiteentry(self, self.site, entry, channels)
+
+    def getentrytimes(self):
+        return explog.explog.getentrytimes(self, self.site)
+
+    def extractgroups(self, base, channelgroups, **kwargs):
         """
-        Looks up the records associated with a particular siteentry
+        Extracts groups of spikes for analysis with klusters. This
+        is the best entry point for analysis. <channelgroups> is
+        either a list of integers or a list of lists of integers.
+        For each member of <channelgroups>, the spikes and
+        features are extracted from the raw pcm_seq2 files. The
+        method writes these files to disk:
+
+        <base>.xml - the parameters file, describes which channels
+                     are in which groups
+        For each group <g>:
+        <base>.spk.<g> - the spike file
+        <base>.fet.<g> - the feature file
+        <base>.clu.<g> - the cluster file (all spikes assigned to one cluster)
         """
-        if channels==None:
-            channels = range(self.nchannels)
-        table = self.elog.root.entries
-        pen,site = self.site
-        rnums = [r.nrow for r in table.where(table.cols.siteentry==entry) \
-                 if r['pen']==pen and r['site']==site and r['channel'] in channels]
-        if len(rnums)==0:
-            raise ValueError, "No entries match the entry and channel set"
 
-        rows =  table.readCoordinates(rnums)
-        #nsamples = rows[0]['duration']
-        #pfp = [self._filecache[r['filebase']] for r in rows]
-        #pcmfiles = [r['filebase'] for r in rows]
-        #entries = [r['entry'] for r in rows]
+        xmlhdr = """<parameters creator="pyklusters" version="1.0" >
+                     <acquisitionSystem>
+                      <nBits>16</nBits>
+                      <nChannels>%d</nChannels>
+                      <samplingRate>20000</samplingRate>
+                      <voltageRange>20</voltageRange>
+                      <amplification>100</amplification>
+                      <offset>0</offset>
+                     </acquisitionSystem>
+                     <fieldPotentials>
+                      <lfpSamplingRate>1250</lfpSamplingRate>
+                     </fieldPotentials>
+                     <spikeDetection>
+                       <channelGroups>
+                  """ % self.nchannels
+        
+        group = 1
+        xmlfp = open(base + ".xml",'wt')
+        xmlfp.write(xmlhdr)
+        for channels in channelgroups:
+            if isinstance(channels, int):
+                channels = [channels]
+            print "Channel group %d: %s" % (group, channels)
 
-        return rows
+            xmlfp.write("<group><channels>\n")
+            for c in channels:
+                xmlfp.write("<channel>%d</channel>\n" % c)
+            xmlfp.write("</channels>\n")
 
-    def getabstimes(self):
-        """
-        The mapping between siteentry and abstime should be one-to-one, so
-        we can use any of the channels.  It's still kind of an expensive
-        operation for huge explogs, so try to only run it once.
-        """
-        pen,site = self.site        
-        table = self.elog.root.entries
-        rnums = [r.nrow for r in table.where(table.cols.site==site) \
-                 if r['pen']==pen and r['channel']==0]
-        if len(rnums)==0:
-            raise ValueError, "No entries for channel 0; something is wrong"
+            spikes, events = self.extractspikes(channels, **kwargs)
+            nsamp = spikes[1].shape[1]
+            writespikes("%s.spk.%d" % (base, group), spikes)
+            
+            xmlfp.write("<nSamples>%d</nSamples>\n" % nsamp)
+            xmlfp.write("<peakSampleIndex>%d</peakSampleIndex>\n" % nsamp)
+            print "Wrote spikes to %s.spk.%d" % (base, group)
 
-        rows =  table.readCoordinates(rnums)
-        out  = {}
-        for r in rows:
-            out[r['siteentry']] = r['abstime']
+            feats = extractfeatures(spikes, events, self.entrytimes)
+            writefeats("%s.fet.%d" % (base, group), feats,
+                          cfile="%s.clu.%d" % (base, group))
+            xmlfp.write("<nFeatures>%d</nFeatures>\n" % ((feats.shape[1] - 1) / len(channels)))
+            xmlfp.write("</group>\n")
+            print "Wrote features to %s.fet.%d" % (base, group)
 
-        return out
+            group += 1
+
+        xmlfp.write("</channelGroups></spikeDetection></parameters>\n")
+        xmlfp.close()
+        print "Wrote parameters to %s.xml" % base
+
 
     def extractspikes(self, channels, **kwargs):
         """
@@ -137,6 +150,7 @@ class site(object):
         pen,site = self.site
         siteentries = set([r['siteentry'] for r in table.where(table.cols.site==site) \
                            if r['pen']==pen])
+
 
         # it doesn't really matter what order we go through the entries
         spikes = {}
@@ -161,28 +175,6 @@ class site(object):
 
         return spikes, events
 
-    def extractfeatures(self, channels, **kwargs):
-        """
-        Calculates principal components of the spike set.
-        """
-        (spikes, events) = self.extractspikes(channels, **kwargs)
-        allspikes = nx.concatenate(spikes.values(), axis=0)
-        pcs = get_pcs(allspikes, **kwargs)
-        n,ndims,nchans = pcs.shape
-        proj = get_projections(allspikes, pcs)
-        # need to calculate time stamps
-        timestamps = nx.zeros((proj.shape[0],1),'l')
-        ##proj.resize((proj.shape[0], proj.shape[1]+1))
-        offset = 0
-        for siteentry, eventlist in events.items():
-            nevents = len(eventlist)
-            timestamps[offset:offset+nevents,0] = eventlist
-            timestamps[offset:offset+nevents,0] += self._timestamps[siteentry]
-            offset += nevents
-            
-        proj.shape = (proj.shape[0], ndims*nchans)
-        return nx.concatenate([proj, timestamps], axis=1)
-    
     def writedat(self, outfile, entry, dtype='h'):
         """
         Exports raw data from an entry to a .dat file.
@@ -195,81 +187,82 @@ class site(object):
         fp = open(outfile, 'wb')
         io.fwrite(fp, signal.size, signal)
         fp.close()
-
-    def writespikes(self, outfile, channelgroup, **kwargs):
-        """
-        Extracts spikes from one or more channels and writes
-        them to kluster's .spk.n files
-
-        Optional arguments get passed to extractspikes()
-        """
-        spikes, events = self.extractspikes(channelgroup, **kwargs)
-        allspikes = nx.concatenate(spikes.values(), axis=0)
-        fp = open(outfile,'wb')
-        io.fwrite(fp, allspikes.size, allspikes.squeeze())
-        fp.close()
-
-    def writefeats(self, outfile, channelgroup, **kwargs):
-        """
-        Measures feature projections of spikes and writes them to disk
-        in the .fet.n format expected by kluster. Can also
-        write a cluster file, assigning all the spikes to the same
-        cluster.
-
-        cfile - the cluster file to write (default none)
-        """
-        feats = self.extractfeatures(channelgroup, **kwargs)
-        fp = open(outfile,'wt')
-        fp.write("%d\n" % feats.shape[1])
-        io.write_array(fp, feats.astype('i'))
-        fp.close()
-        if kwargs.get('cfile',None):
-            fp = open(kwargs.get('cfile'),'wt')
-            for j in range(feats.shape[0]+1):
-                fp.write("1\n")
-            fp.close()
-
-    def writeparams(self, paramfile, groups):
-        """
-        Writes the xml parameter file used by Klusters
-        """
-        pass
     
     def _get_site(self):
         return self._site
 
     def _set_site(self, site):
         self._site = (int(site[0]), int(site[1]))
+        self._timestamps = self.getentrytimes()        
 
     site = property(_get_site, _set_site, None, "The current recording site")
 
-    def __getchannels(self):
-        """
-        Determines the channel set.
-        """
-        self._channels = []
-        table = self.elog.root.channels
-        for r in table:
-            self._channels.append(r['name'])
-
     @property
-    def nchannels(self):
-        return len(self._channels)
-        
-    def __getseqfiles(self):
-        """
-        Returns the seqfiles associated with the current pen/site
-        """
-        table = self.elog.root.entries
-        pen,site = self.site
-        pcmfiles = [r['filebase'] for r in table.where(table.cols.site==site) \
-                    if r['pen']==pen]
-        return set(pcmfiles)
+    def entrytimes(self):
+        return self._timestamps
     
-        
+
+    
+def extractfeatures(spikes, *args, **kwargs):
+    """
+    Calculates principal components of the spike set.
+
+    Provide these two arguments to add a last column with timestamps
+    events - list of lists with event times
+    entrytimes - used to create a last column with timestamps
+    """
+    allspikes = nx.concatenate(spikes.values(), axis=0)
+    pcs = get_pcs(allspikes, **kwargs)
+    n,ndims,nchans = pcs.shape
+    proj = get_projections(allspikes, pcs)
+    proj.shape = (proj.shape[0], ndims*nchans)
+
+    if len(args)==0:
+        return proj
+
+    events, entrytimes = args
+    # need to calculate time stamps
+    timestamps = nx.zeros((proj.shape[0],1),'l')
+    offset = 0
+    for siteentry, eventlist in events.items():
+        nevents = len(eventlist)
+        timestamps[offset:offset+nevents,0] = eventlist
+        timestamps[offset:offset+nevents,0] += entrytimes[siteentry]
+        offset += nevents
+
+    return nx.concatenate([proj, timestamps], axis=1)   
+
+def writespikes(outfile, spikes):
+    """
+    Writes spikes to kluster's .spk.n files
+    """
+    allspikes = nx.concatenate(spikes.values(), axis=0)
+    fp = open(outfile,'wb')
+    io.fwrite(fp, allspikes.size, allspikes.squeeze())
+    fp.close()
+
+def writefeats(outfile, feats, **kwargs):
+    """
+    Measures feature projections of spikes and writes them to disk
+    in the .fet.n format expected by kluster. Can also
+    write a cluster file, assigning all the spikes to the same
+    cluster.
+
+    cfile - the cluster file to write (default none)
+    """
+    fp = open(outfile,'wt')
+    fp.write("%d\n" % feats.shape[1])
+    io.write_array(fp, feats.astype('i'))
+    fp.close()
+    if kwargs.get('cfile',None):
+        fp = open(kwargs.get('cfile'),'wt')
+        for j in range(feats.shape[0]+1):
+            fp.write("1\n")
+        fp.close()
+     
         
 if __name__=="__main__":
 
-    testexplog = 'test.explog.h5'
+    testexplog = '../data/test.explog'
 
     k = site(testexplog,0,0)
