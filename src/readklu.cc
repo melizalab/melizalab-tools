57df114f88824c8010081be6d3b8bd64f9f72a2f
dmeliza <dmeliza@uchicago.edu>
2007-08-17 15:10:03 -0500

    now compile readklu as a python extension module
    darcs-hash:20070817201003-21d25-b5c216f45cd6a891fa522ad94de5fc18de5d290d.gz

--- src/readklu.cc
+++ src/readklu.cc
@@ -1,34 +1,24 @@
-#include<cstdio>
-#include<vector>
-
+#include <Python.h>
+#include "numpy/arrayobject.h"
+#include "CXX/Objects.hxx"
+#include "CXX/Extensions.hxx"
+#include <cstdio>
+#include <vector>
 using std::vector;
 
-int allclust = 0;
-char cname[] = "site_4_2.clu.1";
-char fname[] = "site_4_2.fet.1";
-long _atimes[] = {76134603, 76194603, 76254603, 76314603, 76374603, 76434603,
-		  76494603, 76554603, 76614603, 76674603, 76734603, 76794603};
-int _natimes = 12;
+static vector<vector<vector<long> > >
+readklu(FILE* cfp, FILE* ffp, const Py::List &atimes) 
+{
 
-int main(int argc, char *argv[]) {
-
-	FILE* cfp = fopen(cname, "rt");
-	FILE* ffp = fopen(fname, "rt");
 	int rp = 0;
 	int nclusts, nfeats;
 	int startclust = 0;
-	int nlines = 0;
-
-	// initialize atimes
-	vector<long> atimes;
-	for (int i = 0; i < _natimes; i++) {
-		atimes.push_back((long)_atimes[i]);
-	}
-
+	long nlines = 0;
 
 	// number of clusters and features
 	rp = fscanf(cfp,"%d\n", &nclusts);
 	rp = fscanf(ffp, "%d\n", &nfeats);
+	printf("Clusters: %d\nFeatures:%d\n", nclusts, nfeats);
 	
 	// because we're dropping 0 and 1 (but only if there are more than 1 or 2
 	// units) we have to scan through the cluster file to see if they're there
@@ -55,50 +45,116 @@ int main(int argc, char *argv[]) {
 	if (nclusts>2 && has_one)
 		startclust += 1;
 
-	printf("Total events: %d\n", nlines);
-	printf("Number of clusters: %d\n", nclusts);
-	printf("Number of valid clusters: %d\n", nclusts-startclust);
-
+	printf("Events: %ld\n", nlines);
+	printf("Valid clusters: %d\n", nclusts-startclust);
 	// allocate storage
-	vector<vector<vector<long> > > uvec(nclusts-startclust, 
-					    vector<vector<long> >(atimes.size(), 
-							 vector<long>(0)));
+	int nepisodes = atimes.size();
+	printf("Episodes: %d\n", nepisodes);
+        vector<vector<vector<long> > > uvec(nclusts-startclust, 
+                                            vector<vector<long> >(nepisodes, vector<long>(0)));
 
 	// now iterate through the two files concurrently
-	vector<long>::const_iterator episodetime = atimes.begin();
-	int entry = 0;
-	printf("Episode 0 ");
+	// while matching times to the episode times
+	int episode = 0;
+	long et = Py::Int(atimes[episode]);
+	long nt = Py::Int(atimes[episode+1]);
 	long atime;
+	printf("Start time: %ld\n", et);
+	printf("Episode %d: ", episode);
 	while (rp != EOF) {
  		for (int j = 0; j < nfeats && rp != EOF; j++)
  			rp = fscanf(ffp, "%ld", &atime);
 		rp = fscanf(cfp, "%d\n", &clust);
 		if (clust >= startclust) {
 			// THIS CODE ASSUMES THE ABSTIMES ARE SORTED
- 			if (atime < *episodetime)  
- 				printf("\nWarning: %d comes before the current episode\n", atime);
+ 			if (atime < et)
+ 				printf("\nWarning: %ld comes before the current episode\n", atime);
 			// Advance the pointers until the episodetime is correct
-			while ((episodetime != (atimes.end()-1)) && (atime >= *(episodetime+1))) {
-				entry += 1;
-				episodetime++;
-			}
-			uvec[clust-startclust][entry].push_back(atime - *episodetime);
+ 			while ((nt>0) && (atime >= nt)) {
+ 				episode += 1;
+				printf("\nEpisode %d: ", episode);
+ 				et = nt;
+				if (episode+1 < nepisodes) 
+					nt = Py::Int(atimes[episode+1]);
+				else
+					nt = -1;
+ 			}
+			printf("*");
+			uvec[clust-startclust][episode].push_back(atime - et);
 		}
 	}
-	printf("\n");
-	fclose(cfp);
-	fclose(ffp);
-	
-	for (int i = 0; i < uvec.size(); i++) {
-		printf("Unit %d: [", i);
-		vector<vector<long> > &rvec = uvec[i];
-		for (int j = 0; j < rvec.size(); j++) 
-			printf("%d ", rvec[j].size());
-		printf("]\n");
-	}
 
+	return uvec;
 
 }
 
 
+class _readklu_module : public Py::ExtensionModule<_readklu_module>
+{
+public:
+	_readklu_module()
+		: Py::ExtensionModule<_readklu_module>( "_readklu" )
+		{
+			add_keyword_method("readclusters", &_readklu_module::rk_readclusters, 
+					   "readclusters (fetfile, clufile, eptimes, samplerate=20) - sorts events into episodes");
+                                            
+
+			initialize( "Reads events from klusters files and assigns them to episodes" );
+
+		}
+	virtual ~_readklu_module() {}
+
+private:
+
+	Py::Object
+	rk_readclusters(const Py::Tuple &args, const Py::Dict &kws) {
+
+		FILE *cfp, *ffp;
+		if (args.size() != 3)
+			throw Py::TypeError("readclusters requires 3 arguments.");
+
+		// convert arguments to appropriate types
+		Py::String fname(args[0]);
+		Py::String cname(args[1]);
+		Py::List atimes(args[2]);
+		float samplerate;
+		if (kws.hasKey("samplerate"))
+			samplerate = Py::Float(kws["samplerate"]);
+		else
+			samplerate = 20.0;
+
+		// open the filenames
+		if ((cfp = fopen(cname.as_std_string().c_str(), "rt"))==NULL)
+			throw Py::NameError("Could not open file " + cname.as_std_string());
+		if ((ffp = fopen(fname.as_std_string().c_str(), "rt"))==NULL)
+			throw Py::NameError("Could not open file " + fname.as_std_string());
+				
+  		vector<vector<vector<long> > > uvec = readklu(cfp, ffp, atimes);
+
+		fclose(cfp);
+		fclose(ffp);
+
+ 		// convert output to python lists
+ 		int nunits = uvec.size();
+ 		Py::List ulist;
+ 		for (int i = 0; i < nunits; i++) {
+ 			int nreps = uvec[i].size();
+ 			Py::List rlist;
+ 			for (int j = 0; j < nreps; j++) {
+ 				Py::List events;
+ 				int nevents = uvec[i][j].size();
+ 				for (int k = 0; k < nevents; k++)
+ 					events.append(Py::Float(uvec[i][j][k] / samplerate));
+ 				rlist.append(events);
+ 			}
+ 			ulist.append(rlist);
+ 		}
 	
+		return ulist;
+	}
+};
+
+extern "C" void init_readklu()
+{
+	static _readklu_module* _readklu = new _readklu_module;
+}
--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,33 @@
 from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_lib
-import os
+import os,sys
 
 nxdir = os.path.join(get_python_lib(plat_specific=1), 'numpy/core/include')
+cxxsupp = os.path.normpath(os.path.join(sys.prefix,'share',
+                                        'python%d.%d' % (sys.version_info[0],sys.version_info[1]),
+                                        'CXX') )
+if os.name == 'posix':
+        CXX_libraries = ['stdc++','m']
+else:
+        CXX_libraries = []
 
 _pcmseqio = Extension('_pcmseqio',
                     include_dirs = [nxdir],
                     sources = ['pcmseqio.c','pcmio.c']
                     )
+
+_readklu = Extension('_readklu',
+                     include_dirs = [nxdir],
+                     sources = ['readklu.cc',
+                                os.path.join(cxxsupp,'cxxsupport.cxx'),
+                                os.path.join(cxxsupp,'cxx_extensions.cxx'),
+                                os.path.join(cxxsupp,'cxxextensions.c')])
+
 	
-setup(name = "_pcmseqio",
+setup(name = "dlab",
       version = "1.0",
-      description = "Wrapper for pcm_seq2 read/write functions",
+      description = "Some small extension modules",
       maintainer = "CD Meliza",
       maintainer_email = "dmeliza@uchicago.edu",
-      ext_modules = [ _pcmseqio ]
+      ext_modules = [ _pcmseqio, _readklu ]
       )
