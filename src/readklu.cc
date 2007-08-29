923dc63450b7b421cd871c721ff6034b1338c0dd
dmeliza <dmeliza@gmail.com>
2007-08-28 23:37:45 -0500

    updated readklu.cc to use boost python
    darcs-hash:20070829043745-a299d-ed7ea5a801a467d9bed7e85a97dec0ec4dbdf56d.gz

--- src/readklu.cc
+++ src/readklu.cc
@@ -1,10 +1,14 @@
-#include <Python.h>
-#include "CXX/Objects.hxx"
-#include "CXX/Extensions.hxx"
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/overloads.hpp>
+#include <boost/python/args.hpp>
 #include <cstdio>
 #include <vector>
 #include <set>
 #include <map>
+using namespace boost::python;
 using std::vector;
 using std::set;
 using std::map;
@@ -29,7 +33,7 @@ getclusters(FILE* cfp, set<int> &clusters)
 }
 
 static void
-readklu(FILE* cfp, FILE* ffp, const Py::List &atimes, map<int, vector<vector<long> > > &uvec) 
+readklu(FILE* cfp, FILE* ffp, const list& atimes, map<int, vector<vector<long> > > &uvec) 
 {
 
 	int rp = 0;
@@ -56,7 +60,7 @@ readklu(FILE* cfp, FILE* ffp, const Py::List &atimes, map<int, vector<vector<lon
 	//printf("Events: %ld\n", nlines);
 	//printf("Valid clusters: %d\n", (int)clusters.size());
 	// allocate storage
-	int nepisodes = atimes.size();
+	int nepisodes = len(atimes);
 	uvec.clear();
 	//printf("Episodes: %d\n", nepisodes);
 	for (set<int>::const_iterator it = clusters.begin(); it != clusters.end(); it++) {
@@ -67,8 +71,8 @@ readklu(FILE* cfp, FILE* ffp, const Py::List &atimes, map<int, vector<vector<lon
 	// now iterate through the two files concurrently
 	// while matching times to the episode times
 	int episode = 0;
-	long et = Py::Int(atimes[episode]);
-	long nt = Py::Int(atimes[episode+1]);
+	long et = extract<long>(atimes[episode]);
+	long nt = extract<long>(atimes[episode+1]);
 	long atime;
 	//printf("Start time: %ld\n", et);
 	//printf("Episode %d: ", episode);
@@ -88,7 +92,7 @@ readklu(FILE* cfp, FILE* ffp, const Py::List &atimes, map<int, vector<vector<lon
 				//printf("\nEpisode %d: ", episode);
  				et = nt;
 				if (episode+1 < nepisodes) 
-					nt = Py::Int(atimes[episode+1]);
+					nt = extract<long>(atimes[episode+1]);
 				else
 					nt = -1;
  			}
@@ -99,98 +103,71 @@ readklu(FILE* cfp, FILE* ffp, const Py::List &atimes, map<int, vector<vector<lon
 
 }
 
-
-class _readklu_module : public Py::ExtensionModule<_readklu_module>
+// a wrapper for getclusters
+list rk_getclusters(const std::string& cname) 
 {
-public:
-	_readklu_module()
-		: Py::ExtensionModule<_readklu_module>( "_readklu" )
-		{
-			add_keyword_method("readclusters", &_readklu_module::rk_readclusters, 
-					   "readclusters (fetfile, clufile, eptimes, samplerate=20) - sorts events into episodes");
-			add_varargs_method("getclusters", &_readklu_module::rk_getclusters,
-					   "getclusters (clufile) - returns a list of the clusters defined in the cluters file");
-                                            
-
-			initialize( "Reads events from klusters files and assigns them to episodes" );
-
-		}
-	virtual ~_readklu_module() {}
-
-private:
-
-	Py::Object
-	rk_getclusters(const Py::Tuple &args) {
-		
-		FILE *cfp;
-		Py::String cname(args[0]);
-		if ((cfp = fopen(cname.as_std_string().c_str(), "rt"))==NULL)
-			throw Py::NameError("Could not open file " + cname.as_std_string());
-
-		set<int> clusters;
-		getclusters(cfp, clusters);
-
-		fclose(cfp);
-		Py::List out;
-		for (set<int>::const_iterator it = clusters.begin(); it != clusters.end(); it++)
-			out.append(Py::Int(*it));
-
-		return out;
-	}
-		
-
-	Py::Object
-	rk_readclusters(const Py::Tuple &args, const Py::Dict &kws) {
-
-		FILE *cfp, *ffp;
-		if (args.size() != 3)
-			throw Py::TypeError("readclusters requires 3 arguments.");
-
-		// convert arguments to appropriate types
-		Py::String fname(args[0]);
-		Py::String cname(args[1]);
-		Py::List atimes(args[2]);
-		float samplerate;
-		if (kws.hasKey("samplerate"))
-			samplerate = Py::Float(kws["samplerate"]);
-		else
-			samplerate = 20.0;
-
-		// open the filenames
-		if ((cfp = fopen(cname.as_std_string().c_str(), "rt"))==NULL)
-			throw Py::NameError("Could not open file " + cname.as_std_string());
-		if ((ffp = fopen(fname.as_std_string().c_str(), "rt"))==NULL)
-			throw Py::NameError("Could not open file " + fname.as_std_string());
-				
-  		map<int, vector<vector<long> > > uvec;
-		readklu(cfp, ffp, atimes, uvec);
-
-		fclose(cfp);
-		fclose(ffp);
-
- 		// convert output to python lists
- 		Py::List ulist;
- 		for (map<int, vector<vector<long> > >::const_iterator it = uvec.begin();
-		     it != uvec.end(); it++) {
-			int unit = it->first;
-			//printf("cluster %d.\n", unit);
- 			int nreps = uvec[unit].size();
- 			Py::List rlist;
- 			for (int j = 0; j < nreps; j++) {
- 				Py::List events;
- 				int nevents = uvec[unit][j].size();
- 				for (int k = 0; k < nevents; k++)
- 					events.append(Py::Float(uvec[unit][j][k] / samplerate));
- 				rlist.append(events);
- 			}
- 			ulist.append(rlist);
- 		}
 	
-		return ulist;
-	}
-};
+	FILE *cfp;
+	if ((cfp = fopen(cname.c_str(), "rt"))==NULL)
+		throw std::ios_base::failure("Could not open file " + cname);
 
-extern "C" void init_readklu()
-{
-	static _readklu_module* _readklu = new _readklu_module;
+	set<int> clusters;
+	getclusters(cfp, clusters);
+
+	fclose(cfp);
+	list out;
+	for (set<int>::const_iterator it = clusters.begin(); it != clusters.end(); it++)
+		out.append(*it);
+
+	return out;
 }
+
+ 
+list rk_readclusters(const std::string& fname, const std::string& cname,
+		     const list& atimes, float samplerate=20.0)
+{
+	
+	FILE *cfp, *ffp;
+	
+	// open the filenames
+	if ((cfp = fopen(cname.c_str(), "rt"))==NULL)
+		throw std::ios_base::failure("Could not open file " + cname);
+	if ((ffp = fopen(fname.c_str(), "rt"))==NULL)
+		throw std::ios_base::failure("Could not open file " + fname);
+				
+	map<int, vector<vector<long> > > uvec;
+	readklu(cfp, ffp, atimes, uvec);
+
+	fclose(cfp);
+	fclose(ffp);
+
+	// convert output to python lists
+	list ulist;
+	for (map<int, vector<vector<long> > >::const_iterator it = uvec.begin();
+	     it != uvec.end(); it++) {
+		int unit = it->first;
+		//printf("cluster %d.\n", unit);
+		int nreps = uvec[unit].size();
+		list rlist;
+		for (int j = 0; j < nreps; j++) {
+			list events;
+			int nevents = uvec[unit][j].size();
+			for (int k = 0; k < nevents; k++)
+				events.append(uvec[unit][j][k] / samplerate);
+			rlist.append(events);
+		}
+		ulist.append(rlist);
+	}
+	
+	return ulist;
+}
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(rk_readclusters_overloads, rk_readclusters, 3, 4)
+
+BOOST_PYTHON_MODULE(_readklu)
+{
+	def("getclusters", rk_getclusters);
+	def("readclusters", rk_readclusters, rk_readclusters_overloads(
+		    args("samplerate")));
+}
+		
