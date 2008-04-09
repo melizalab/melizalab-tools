ac5b0c978808bde4555e667fa4f7b86a397ae8ed
dmeliza <dmeliza@uchicago.edu>
2008-04-09 16:25:27 -0500

    featureresponses.py: working simple linear model
    darcs-hash:20080409212527-21d25-6207319aab365e0098e9eac572966d77b52fa44f.gz

--- features/featureresponses.py
+++ features/featureresponses.py
@@ -8,7 +8,9 @@ Collect responses to features in feature noise.
 from __future__ import with_statement
 import os,sys,glob
 import numpy as nx
+from scipy import sparse, linalg
 from dlab import toelis, plotutils
+from dlab.pointproc import kernrates
 from motifdb import db
 
 
@@ -16,7 +18,7 @@ from motifdb import db
 feature_db = os.path.join(os.environ['HOME'], 'z1/stimsets/songseg/motifs_songseg.h5')
 featset = 0
 # where to look up the feature locations in feature noise
-featloc_tables = os.path.join(os.environ['HOME'], 'z1/stimsets/songseg/acute')
+featloc_tables = os.path.join(os.environ['HOME'], 'z1/stimsets/songseg/feattables')
 
 
 def readftable(filename, mdb, Fs=20.):
@@ -30,6 +32,44 @@ def readftable(filename, mdb, Fs=20.):
 
     return nx.rec.fromrecords(rows, names='motif,feature,fstart,flen')
 
+def ftabletomatrix(ftable, binsize=50, nlags=1, tmax=None, meancol=False):
+    """
+    Convert a feature table to a matrix in which each column is a feature,
+    each row is a time bin, and the value of the matrix is 1 at the feature onset
+    (rounding down to the nearest time bin start) and 0 otherwise.
+
+    meancol - if true, includes a column of all zeros for mean firing rate.
+
+    Returns (M,F,T)
+    M - feature matrix (2D sparse array)
+    F - feature names for each column (list)
+    T - time bin onsets
+    """
+
+    lastfeat = ftable['fstart'].argmax()
+    if tmax == None:
+        tmax = ftable[lastfeat]['fstart'] + ftable[lastfeat]['flen']
+    nfeats = len(ftable)
+
+    T = nx.arange(0, nx.ceil(tmax / binsize) * binsize, binsize)
+    F = []
+
+    nrow,ncol = T.size, nfeats * nlags
+    #M = nx.zeros((T.size, len(ftable)), dtype=nx.int8)
+    M = sparse.lil_matrix((nrow, ncol))
+
+    for i,feat in enumerate(ftable):
+        F.append('%(motif)s_%(feature)d' % feat)
+        for j in range(nlags):
+            jj = int(feat['fstart'] / binsize)+j
+            if jj < M.shape[0]:
+                M[jj, i*nlags+j] = 1
+
+    if meancol:
+        M = sparse.hstack([nx.ones((nrow,1)), M])
+    return M.tocsr(),F,T
+    
+
 def splittoelis(tl, feattbl, postpad=200.):
     """ Run through a toelis and split it into features """
 
@@ -40,17 +80,33 @@ def splittoelis(tl, feattbl, postpad=200.):
 
     return tls
 
-def collectfeatures(song, mdb, **kwargs):
+def loadresponses(song, **kwargs):
+    """
+    Loads all the feature noise responses associated with a song
+    (e.g. C0_densefeats_000.toe_lis)
+    
+    Optional arguments:
+    dir - search for files in this directory
+    """
+    
+    glb = os.path.join(kwargs.get('dir',''), '*%s*feats*.toe_lis' % song)
+    files = glob.glob(glb)
+    tls = {}
+    for file in files:
+        stimname = file[file.find(song):-8]
+        tl = toelis.readfile(file)
+        tls[stimname] = tl
+
+    return tls
+
+def collectfeatures(response_tls, mdb, **kwargs):
     """
     Run through all the toelis data for a song, and collect the
     responses to features. Currently only indexed by primary feature.
     """
-    files = glob.glob('*%s*feats*.toe_lis' % song)
     tls = []
-    for file in files:
-        stimname = file[file.find(song):-8]
+    for stimname, tl in response_tls.items():
         ftable = readftable(os.path.join(featloc_tables, stimname + '.tbl'), mdb, **kwargs)
-        tl = toelis.readfile(file)
         tls.append(splittoelis(tl, ftable, **kwargs))
 
     # group by key; assume that nothing is broken and just use the keys in the first toelis dict
@@ -79,8 +135,11 @@ def plotresps(tls, mdb, bandwidth=5, plottitle=None,
     maxrate = 0
     maxdur = 0
     fdurs = []
+    feats = tls.keys()
+    feats.sort()
         
-    for feature,tl in tls.items():
+    for feature in feats:
+        tl = tls[feature]
         a = f.add_subplot(ny, 3, pnums[plotnum])
         a.hold(True)
         motname, featnum = feature.split('_')
@@ -97,9 +156,7 @@ def plotresps(tls, mdb, bandwidth=5, plottitle=None,
         else:
             r,g = kernrates(tl, 1.0, bandwidth, 'gaussian', onset=padding[0],
                             offset=fdur+padding[1])
-            r = r.mean(1)
-            #b,v = tl.histogram(binsize=1.,normalize=1)
-            #smooth_v = gaussian_filter1d(v.astype('f'), bandwidth)
+            r = r.mean(1) * 1000
             maxrate = max(maxrate, r.max())
             fdurs.append(fdur)
             a.plot(g,r,'b')
@@ -119,7 +176,7 @@ def plotresps(tls, mdb, bandwidth=5, plottitle=None,
         a = ax[i]
         if not rasters:
             fdur = fdurs[i]
-            a.plot([0,0],[0,maxrate],'k:',[fdur,fdur],[0,maxrate],'k:')
+            a.plot([0,0],[0,maxrate],'k',[fdur,fdur],[0,maxrate],'k')
         
         a.set_xlim((padding[0], maxdur+padding[1]))
 
@@ -129,7 +186,60 @@ def plotresps(tls, mdb, bandwidth=5, plottitle=None,
     f.subplots_adjust(hspace=0.)
     return f    
 
+def resprate(tl, binsize):
+    #b,f = tl.histogram(binsize=binsize, onset=0)
+    #return f
+    r,g = kernrates(tl, 2, 15, 'gaussian', onset=0, gridspacing=binsize)
+    return r.mean(1) * 1000
+
 if __name__=="__main__":
     
     mdb = db.motifdb(feature_db)
     exampledir = "/z1/users/dmeliza/acute_data/st358/20080129/cell_4_2_2"
+    song = 'C0'
+    binsize = 30
+    nlags = 10
+    meancol = True
+
+    rtls = loadresponses(song, dir=exampledir)
+    tls = collectfeatures(rtls, mdb)
+
+    R = []
+    MM = []
+    for stim, tl in rtls.items():
+        ftable = readftable(os.path.join(featloc_tables, stim + '.tbl'), mdb)
+        tmax = tl.range[1]
+        f = resprate(tl, binsize)
+        M,F,T = ftabletomatrix(ftable, binsize=binsize,
+                               nlags=nlags, tmax=tmax, meancol=meancol)
+        R.append(f)
+        MM.append(M)
+
+    Y = nx.concatenate(R)
+    X = sparse.vstack(MM).tocsr()
+
+    CSS = X.T * X
+    CSR = X.T * Y
+    A = sparse.linalg.spsolve(CSS, CSR)
+    Yhat = A * X.T
+
+    print "Fit CC: %3.4f" % nx.corrcoef(Y, Yhat)[0,1]
+    if meancol:
+        print "Mean FR (fit): %3.4f" % A[0]
+    
+    # reshape
+    startind = 1 if meancol else 0
+    Amat = nx.reshape(A[startind:], ((A.size-startind) / nlags, nlags)).T
+    t = nx.arange(0,Amat.shape[0]*binsize,binsize)
+
+    # x-validate
+    songtl = toelis.readfile(os.path.join(exampledir,'cell_4_2_2_C0.toe_lis'))
+    f = resprate(songtl, binsize)
+    ftab = readftable(os.path.join(featloc_tables, 'C0.tbl'),mdb)
+    Msong,F,T = ftabletomatrix(ftab, binsize=binsize,
+                               nlags=nlags, tmax=songtl.range[1], meancol=meancol)
+    fhat = A * Msong.T
+    print "Song CC: %3.4f" % nx.corrcoef(f, fhat)[0,1]
+
+    
+    
