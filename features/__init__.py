1b9ffaea62c053a3fc8260e738e457110beed40c
dmeliza <dmeliza@uchicago.edu>
2008-09-25 16:48:29 -0500

    features.__init__ added
    darcs-hash:20080925214829-21d25-e8ab6cf61b4991141afc1f541811a7619212927d.gz

---
+++ features/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+"""
+Modules in this package contain functions for analyzing feature
+content of stimuli, and the responses to the features.
+"""
+
+__all__ = ['featurebreakdown','featureresponses','findfeatures',
+           'songanalysis','songfeatures','procmotifs','preprocess']
