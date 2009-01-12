96d52ab855401993699f12c47cb9672ba9c9a572
dmeliza <dmeliza@uchicago.edu>
2009-01-12 10:12:08 -0600

    features.genuniqueperms: add flush command and option for starting permutation
    darcs-hash:20090112161208-21d25-1682761762ec3d6fb500b8c4546d2ab64c13c360.gz

--- features/genuniqueperms.py
+++ features/genuniqueperms.py
@@ -3,8 +3,11 @@
 """
 Generate transition-unique permutations and dump them to a file
 
-Usage: genuniqueperms.py N
+Usage: genuniqueperms.py N [K]
 
+Arguments:
+       N - number of elements in the sequences
+       K - starting permutation (default 0)
 Output:
        N_transperms.tbl - N+1 column table; first column has the permutation number
 
@@ -22,10 +25,13 @@ if __name__=="__main__":
         sys.exit(-1)
 
     N = int(sys.argv[1])
+    K = 0 if len(sys.argv) < 3 else sys.argv[2]
+    
     with open('%d_transperms.tbl' % N, 'wt') as fp:
         for x,perm in datautils.perm_unique_trans(N):
             print "permutation %d is unique..." % x
             fp.write('%d' % x)
             for y in perm: fp.write('\t%d' % y)
             fp.write('\n')
+            fp.flush()
 
