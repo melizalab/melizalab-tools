9730faa6e8c97a4a80acd9a321529b593cee76d9
dmeliza <dmeliza@uchicago.edu>
2009-02-09 17:29:14 -0600

    features.songfeatures: output different stuff for different featsets
    darcs-hash:20090209232914-21d25-7af4ae266c6cabfbccbc3d6a290695a52a7c40ce.gz

--- features/songfeatures.py
+++ features/songfeatures.py
@@ -4,20 +4,23 @@
 Generate tables of feature locations for songs and motifs. This
 can be done using the offset data stored in the motif database.
 
-Usage: songfeatures.py <songs>
+Usage: songfeatures.py [-f <fset] <songs>
 
 For each song argument, determines the offsets of each
 feature in the song and component motifs and outputs a .tbl
 file with the same format as the .tbl files for the feature noise
+
+-f <fset>  : specify which feature set to use (default 0)
 """
 
-import os, sys
+import os, sys, getopt
 from motifdb import db
 
 mdbfile = os.path.join(os.environ['HOME'], 'z1/stimsets/songseg', 'motifs_songseg.h5')
 Fs = 20.
+featset = 0
 
-def songftable(song, mdb):
+def songftable(song, mdb, featset=0):
 
     # this code is basically copied from featurebreakdown
     motifs = mdb.get_motifs()
@@ -33,8 +36,7 @@ def songftable(song, mdb):
             # figure out the start and stop of the motif (note: in ms)
             motstart = float(motfile.split('_')[-2])
             
-
-            feats = mdb.get_features(motif)
+            feats = mdb.get_features(motif, featset)
             for feat in feats:
                 offset = feat['offset'][0]
                 ftables[motif].append([mdb.get_symbol(feat['motif']),
@@ -47,18 +49,25 @@ def songftable(song, mdb):
                 
 if __name__=="__main__":
 
-    if len(sys.argv) < 2:
+    opt,songs = getopt.getopt(sys.argv[1:],'f:')
+    for o,a in opt:
+        if o=='-f':
+            featset = int(a)
+
+    if len(songs) < 1:
         print __doc__
         sys.exit(-1)
-
-    songs = sys.argv[1:]
+        
     mdb = db.motifdb(mdbfile)
 
     for song in songs:
 
-        ftables = songftable(song, mdb)
+        ftables = songftable(song, mdb, featset)
+        # drop all the minor tables
+        ftables = {song : ftables[song]}
+        
         for stim,ftable in ftables.items():
-            fp = open('%s.tbl' % stim, 'wt')
+            fp = open('%s_%d_recon.tbl' % (stim, featset), 'wt')
             for feat in ftable:
                 feat[2] *= Fs  # use samples to match other table format
                 fp.write('%s\t%d\t%d\t1.0\n' % tuple(feat))
