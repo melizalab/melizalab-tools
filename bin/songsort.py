1d7f55a90e1afc5b0f80ea05add802c49c721da2
dmeliza <dmeliza@uchicago.edu>
2009-05-05 17:07:02 -0500

    songsort.py: no longer uses explog class; splits output files by date
    darcs-hash:20090505220702-21d25-8d645bedb071d950e430b67347c0dd0ff135a61e.gz

--- bin/songsort.py
+++ bin/songsort.py
@@ -3,7 +3,7 @@
 """
 songsort.py [-m <minlength] <explog>
 
--m         Set minimum song length (default 
+-m         Set minimum song length (default 25 s)
 
 This script attempts to compensate for some of the issues arising when
 trying to record starling song with saber.  The automatic triggering
@@ -17,15 +17,17 @@ scanning through the explog and locating episodes that have no space
 between them. It stitches these toegether.  At the same time, it
 checks the length of the (stitched) entries against some minimum
 length and sorts out things that are too short.  The good episodes are
-written to a new pcm_seq2 file, and the questionable episodes to another.
+written to new pcm_seq2 files.
 
 """
 
-import os, sys, getopt
+import os, sys, getopt, datetime
 import numpy as nx
 from dlab import datautils
-from mspikes import explog, _pcmseqio
+from mspikes import _pcmseqio
+from mspikes.explog import _reg_create, _reg_triggeron, _reg_triggeroff
 
+__version__ = 1.0.2
 song_min_length = 25. # seconds
 
 if __name__=="__main__":
@@ -41,70 +43,100 @@ if __name__=="__main__":
 
     elogfile = args[0]
 
-    # the explogs for song recording are quite short, so just read the text file
-    e = explog.readexplog(elogfile, elogfile + '.h5')
+    print "# songsort.py version %s" % __version__
+    print "# input: %s" % elogfile
+    print "# min song length: %f" % song_min_length
 
-    # open a filecache for input files
-    fcache = datautils.filecache(_pcmseqio.pcmfile)
+    # the explogs for song recording are generally short, so just read the text file
+    elog = open(elogfile,'r')
 
-    # open the pcmseq2 files for output
-    base,ext = os.path.splitext(elogfile)
-    fname_song = base + '_song.pcm_seq2'
-    fname_nois = base + '_noise.pcm_seq2'
-    
-    fp_song = _pcmseqio.pcmfile(fname_song, 'w')
-    fp_nois = _pcmseqio.pcmfile(fname_nois, 'w')
-
+    fname_base,ext = os.path.splitext(elogfile)
+    fname_out = ''
     laststop = 0L
     lastdata = []
-    for entry in e:
-        start = entry['abstime']
-        stop = start + entry['duration']
-        fentry = e.getfiles(start)[0]
-        if not os.path.exists(fentry['filebase']):
-            continue
-        fp = fcache[fentry['filebase']]
-        fp.entry = fentry['entry']
-        sout = "%s/%d: start %d, stop %d" % (fentry['filebase'], fentry['entry'],
-                                             start, stop)
-
-        elen = 1. * fp.nframes / fp.framerate
-        S = fp.read()
-        sigmax = 2**(S.dtype.itemsize*8-1) - 10
-        if S.max() >= sigmax or S.min() <= -sigmax:
-            print sout + " -> clipped; ignoring"
-            continue
-
-        if len(lastdata) > 0 and start > laststop:
-            # if the buffer has data, and there is a gap since the last episode,
-            # write the buffer to disk
-            data = nx.concatenate(lastdata)
-            # check to see if it's long enough to consider song
-            elen = 1. * data.size / fp.framerate
-            if elen >= song_min_length:
-                fp_out = fp_song
-                fname_out = fname_song
+    fp = None
+    # I wind up duplicating some of the stuff in explog.readexplog, but I think
+    # it's better not to try to shoehorn these explogs, which can stretch over many days,
+    # into a file format intended for multichannel recordings. This does restrict us to
+    # mono recordings...
+    for line_num, line in enumerate(elog):
+        lstart = line[0:4]
+        if lstart=='FFFF':
+            m1 = _reg_create.search(line)
+            if m1:
+                if m1.group('action')=='created':
+                    seqfile = m1.group('file')
+                    if os.path.exists(seqfile):
+                        seqbase,ext = os.path.splitext(seqfile)
+                        fp = _pcmseqio.pcmfile(seqfile,'r')
+                        print "%s: opened" % seqfile
+                    else:
+                        fp = None
+                        print "%s: unable to find, skipped" % seqfile
             else:
-                fp_out = fp_nois
-                fname_out = fname_nois
+                print "explog error: Unparseable FFFF line (%d): %s" % (line_num, line)
 
-            if fp_out.nframes > 0:
-                fp_out.entry += 1
-                
-            fp_out.timestamp = lasttimestamp
-            fp_out.framerate = fp.framerate  # this shouldn't be different
-            fp_out.write(data)
-            print "Wrote %d entries to entry %d of %s" % (len(lastdata), fp_out.entry, fname_out)
+        elif lstart=='TTTT':
+            # skip these entries until we have a valid open file
+            if fp==None:
+                continue
+            m1 = _reg_triggeron.search(line)
+            m2 = _reg_triggeroff.search(line)
+            if m1==None and m2==None:
+                print "parse error: Unparseable TTTT line (%d): %s" % (line_num, line)
+                continue
+            elif m1:
+                # trigger on
+                start = int(m1.group('onset'))
+                chan = m1.group('chan')
+                entry = int(m1.group('entry'))
+            elif m2:
+                # trigger off
+                if m2.group('chan')!= chan or int(m2.group('entry'))!= entry:
+                    print "warning: found SONG_OFF for entry %d but last SONG_ON entry was %d (line %d)" %\
+                          (m2.group('entry'), entry, line_num)
+                    continue
+                n_samples = int(m2.group('samples'))
+                stop = start + n_samples
 
-            # now reinitialize the buffer, etc
-            lastdata = []
+                # process the entry
+                sout = "%s/%d: start %d, stop %d" % (seqbase, entry, start, stop)
 
-        print sout
-        lastdata.append(S)
-        laststop = stop
-        lasttimestamp = fp.timestamp
+                fp.entry = entry
+                S = fp.read()
+                sigmax = 2**(S.dtype.itemsize*8-1) - 10
+                if S.max() >= sigmax or S.min() <= -sigmax:
+                    print sout + " -> clipped; ignoring"
+                    continue
+                # if the buffer has data, and there is a gap since the last episode,
+                # write the buffer to disk
+                if len(lastdata) > 0 and start > laststop:
+                    data = nx.concatenate(lastdata)
+                    # check to see if it's long enough to consider song
+                    elen = 1. * data.size / fp.framerate
+                    if elen >= song_min_length:
+                        # it's safe to call this b/c the first entry will never get written
+                        ff = '%s_%s_song.pcm_seq2' % (fname_base, file_tstamp.strftime('%Y%m%d'))
+                        if not ff==fname_out:
+                            fname_out = ff
+                            fp_song = _pcmseqio.pcmfile(fname_out,'w')
+                            print "* %s: opened for output" % fname_out
+                        if fp_song.nframes > 0:
+                            fp_song.entry += 1
 
+                        fp_song.timestamp = lasttimestamp
+                        fp_song.framerate = fp.framerate  # this shouldn't be different
+                        fp_song.write(data)
+                        print "* %s/%d: wrote %d entries" % (fname_out, fp_song.entry, len(lastdata))
+                    else:
+                        print "* discarded last %d entries (too short)" % len(lastdata)
 
-    del(e)
-    del(fp_song)
-    del(fp_nois)
+                    # now reinitialize the buffer, etc
+                    lastdata = []
+
+                print sout
+                lastdata.append(S)
+                laststop = stop
+                lasttimestamp = fp.timestamp
+                file_tstamp = datetime.datetime.utcfromtimestamp(fp.timestamp)
+
