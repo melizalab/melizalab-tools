83535fbc1dc1255d75cbd3c57531a486456a36c1
dmeliza <dmeliza@dylan.uchicago.edu>
2010-06-08 18:01:14 -0500

    moved discrete convolution code into its own c file with f2py interface
    Ignore-this: f9cb8ae4b61276919e1e7f6eec3db10f
    
    darcs-hash:20100608230114-17637-ff4bffb32e848e5a5b88bbab1ccbe93a298a6db5.gz

---
+++ src/convolve.c
@@ -0,0 +1,53 @@
+/* @(#)convolve.c
+ * 
+ * Functions for computing convolutions
+ */
+#include <math.h>
+
+void
+discreteconv(const double *times, int ntimes, const double *kern, int nkern, 
+	     double kdt, double onset, double offset, double odt,
+	     double *out)
+{
+	int ipt;
+	int NT = (int)ceil((offset - onset) / odt);
+	double W_dur = nkern*kdt;
+
+	for (ipt = 0; ipt < ntimes; ipt++) {
+		double cur_point = times[ipt];
+
+		// check that the time is within the analysis window
+		if (cur_point < onset || cur_point > offset)
+			continue;
+
+		// compute time relative to the initial point of the grid
+		cur_point -= onset;
+
+		// find the nearest grid point to the left of the current point
+		int dt = (int)floor( cur_point / odt);
+
+		// If function f is tranlated to the time grid point, the relative
+		// location of the current point in the support of the function f
+		double rel_loc = cur_point - odt * (double)dt;
+
+		while (rel_loc <= W_dur && dt >= 0) {
+                    /* Use linear interpolation to compute the change
+                       to each time grid point.
+
+		       Among the grid points for function [f], find
+                       the right most one to the left to the current
+                       point.
+                    */
+                    double dR = rel_loc / kdt;
+                    int  drel_loc = (int)floor(dR);
+                    if (dt < NT && drel_loc < nkern ) {
+                            double dx = dR - drel_loc;
+                            out[dt] += (1.0 - dx) * kern[drel_loc];
+                            if ( drel_loc < nkern - 1) 
+                                    out[dt] += dx * kern[drel_loc+1];
+                    }
+                    dt --;
+                    rel_loc += odt;
+            }
+    }
+}
