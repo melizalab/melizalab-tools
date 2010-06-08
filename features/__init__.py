1d506c0a601fa774ac6a3bc71f72a1492bcc0b99
dmeliza <dmeliza@dylan.uchicago.edu>
2010-06-08 14:46:52 -0500

    disposed of features subdir
    Ignore-this: 6c0d5195e42b1d5c264a1e7b193448e1
    
    darcs-hash:20100608194652-17637-af564b97cde2962b31f5dc8e566ef7fc9d959bd6.gz

--- features/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-"""
-Modules in this package contain functions for analyzing feature
-content of stimuli, and the responses to the features.
-"""
-
-__all__ = ['featurebreakdown','featureresponses','findfeatures',
-           'songanalysis','songfeatures','procmotifs','preprocess']
