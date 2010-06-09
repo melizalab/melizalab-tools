068ef35594e92e24bc7c48e9746a813860122648
dmeliza <dmeliza@dylan.uchicago.edu>
2010-06-09 12:16:18 -0500

    stats.py: cleanup
    Ignore-this: 7418a9ac793ac0c60d3e29e1a7084bf
    
    darcs-hash:20100609171618-17637-170b85998dbd6ffff13fdd1a6274e55552019b0c.gz

--- dlab/stats.py
+++ dlab/stats.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 """
-Statistical tools not found in the scipy or numpy toolkits.  
+Statistical tools not found in the scipy or numpy toolkits.
+
+Functions
+=====================================
+rmvnorm:           sample from gaussian distribution with known covariance
+T1_test:           test sample covariance matrix from known cov matrix
+corrcoef_ci:       confidence interval for correlation coeffient
+ptiles:            look up percentiles in empirical CDF
 
 Copyright (C) 2009 Daniel Meliza <dmeliza@dylan.uchicago.edu>
 Created 2009-09-03
@@ -14,6 +21,8 @@ def generate_multivariate_gaussian(covariance):
     Randomly sample from a gaussian distribution with known covariance
     structure. Returns an infinite iterator that yields a new sample
     from the distribution with each iteration.
+
+    covariance:  covariance matrix (must be square)
     """
     from numpy import dot, shape
     from numpy.random import normal
@@ -31,17 +40,22 @@ def T1_test(sample_cov,true_cov,n):
     Test the hypothesis that a sample covariance matrix comes from a
     multivariate normal distribution whose true covariance is given
 
-    Returns the probability of obtaining a covariance matrix like this 
+    sample_cov:    sample covariance matrix
+    true_cov:      known covariance matrix
+    n:             number of observations per variable
+
+    Returns the probability of obtaining a covariance matrix like this
     if the distribution were multivariate normal.
 
-    Based on Nagao 1973, this is true only for n large (and larger than the size of the matrix).
+    Based on Nagao 1973, this is true only for n large (and larger
+    than the size of the matrix).
 
     By Anne M. Archibald 2007
     """
     from numpy import dot, shape, trace, eye
     from scipy.linalg import inv
     from scipy.stats import chi2
-    
+
     p, r = shape(sample_cov)
     if p!=r or (p,r) != shape(true_cov):
         raise ValueError, "Sample covariance matrix (%d by %d) and true covariance matrix (%d by %d) must be square matrices of the same size" % (p,r,shape(true_cov)[0],shape(true_cov)[1])
@@ -56,27 +70,25 @@ def T1_test(sample_cov,true_cov,n):
                                       p/2.*(p+1)**2*chi2(f+2).cdf(T1)-
                                       p/24.*(2*p**2+3*p-1)*chi2(f).cdf(T1))
 
-def assert_T1_test(sample_cov,true_cov,n,p=1e-3):
-    T1p = T1_test(sample_cov,true_cov,n)
-    assert T1p>p, "Sample covariance %s with %d points does not match true covariance %s: probability %g<%g" % (sample_cov, n, true_cov, T1p, p)
 
-
-def corrcoef_interval(x,y,alpha=0.05):
+def corrcoef_ci(x,y,alpha=0.05):
     """
     Pearson product-moment correlation between x and y with confidence
     intervals and p-value.
+
+    x,y:   variables to correlate. Must be same size.
 
     Returns r, r_upper, r_lower, r_p
     """
     from numpy import sqrt, tanh, arctanh
     from scipy.stats import pearsonr, norm
-    
+
     assert x.size == y.size, "Input vectors must be the same length"
     N = x.size
     Z = lambda r: sqrt(N-3) * arctanh(r)
     IZ = lambda z: tanh(z / sqrt(N-3))
     ci = norm.isf(alpha/2)
-    
+
     r,r_p = pearsonr(x,y)
     z = Z(r)
     return r, IZ(z+ci), IZ(z-ci), r_p
@@ -85,7 +97,9 @@ def ptiles(x, p, na_rm=True):
     """
     Return values of x at each percentile in p.
 
-    na_rm - if True (default), remove nans and Infs first; if all values nan, return nan
+    x:     vector of samples
+    p:     sequences of percentiles to test
+    na_rm: if True (default), remove nans and Infs first; if all values nan, return nan
     """
     from numpy import isfinite, nan, ones, asarray
     from scipy.stats import scoreatpercentile
