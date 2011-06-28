78f7ab6ff7414070614fa83ee0f15d125bab59cc
dmeliza <dmeliza@dylan.uchicago.edu>
2011-06-28 10:56:29 -0500

    io.py: remove dep on decorator; add readbin()
    Ignore-this: 3870e4c84dfe593132ef7a69deac5e5b
    
    darcs-hash:20110628155629-17637-19aeb6f45c3dc3f90999c2c58ef0f72f13edf059.gz

--- dlab/io.py
+++ dlab/io.py
@@ -11,19 +11,32 @@ read_table:       load data in tabular format with custom conversion
 Copyright (C) 2010 Daniel Meliza <dmeliza@dylan.uchicago.edu>
 Created 2010-12-01
 """
-from decorator import decorator
+from functools import wraps
 
 def skipcomments(f):
     """ Fix broken numpy.recfromtxt with respect to locating header """
-    def _read(func, fname, *args, **kw):
+    @wraps(f)
+    def _read(fname, *args, **kw):
         comment = kw.get("comment","#")
         with open(fname,'rt') as fp:
             for lnum,l in enumerate(fp):
                 if len(l)>0 and l[0]!=comment: break
         kw["skip_header"] = lnum if kw.get("names",None) is True else lnum+1
-        return func(fname, *args, **kw)
-    return decorator(_read, f)
+        return f(fname, *args, **kw)
+    return _read
 
-
+def readbin(fname, dtype='d'):
+    """
+    Read general matrix binary format, which consists of two integers
+    indicating the data dimensions followed by the contents of the
+    matrix, in C order.
+    """
+    from numpy import fromfile
+    with open(fname,'rb') as fp:
+        dims = fromfile(fp,dtype='i',count=2)
+        data = fromfile(fp,dtype=dtype)
+        data.shape = dims
+        return data
+    
 # Variables:
 # End:
