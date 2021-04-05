3e7e95dae5b7fce3216b22948483167975b8dfe1
Dan Meliza <dan@meliza.org>
2021-04-05 15:37:51 -0400

    start work on kilosort scripts

--- dlab/pprox.py
+++ dlab/pprox.py
@@ -55,3 +55,36 @@ def validate(obj):
     import jsonschema
     import requests
     pass
+
+
+def trial_iterator(pprox):
+    """ Iterates through trials in pprox, yielding (index, trial) """
+    for i, trial in enumerate(pprox["pprox"]):
+        # annotate with sampling rate
+        if "sampling_rate" not in trial["recording"]:
+            entry = trial["recording"]["entry"]
+            sampling_rate = pprox["entry_metadata"][entry]["sampling_rate"]
+            trial["recording"]["sampling_rate"] = sampling_rate
+        yield i, trial
+
+
+def aggregate_events(pprox, use_recording=False):
+    """Aggregate all the events in a pprox into a single array.
+
+    This function is primarily used for testing, as this should be the reverse
+    operation to whatever function is used to assign events to trials (assuming
+    no gaps in the recording). If there are gaps, then set `use_recording` to
+    True.
+
+    """
+    import numpy as np
+    all_events = []
+    for trial in pprox["pprox"]:
+        sampling_rate = trial["recording"]["sampling_rate"]
+        events = np.asarray(trial["events"])
+        if use_recording:
+            events = (events * sampling_rate).astype("i8") + trial["recording"]["start"]
+        else:
+            events = ((events + trial["offset"]) * sampling_rate).astype("i8")
+        all_events.append(events)
+    return np.concatenate(all_events)
