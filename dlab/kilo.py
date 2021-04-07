06bcd6d8739f7d9592f7dee71b48134009337ed2
Dan Meliza <dan@meliza.org>
2021-04-07 15:01:37 -0400

    work on group-kilo-spikes

--- dlab/kilo.py
+++ dlab/kilo.py
@@ -12,8 +12,8 @@ from dlab import core, __version__
 log = logging.getLogger("dlab.kilo")
 
 
-def assign_events(pprox, events):
-    """Assign events to trials within a pprox based on recording time.
+def assign_events(pprox, event_times, event_clusts, warn_unassigned=False):
+    """Assign event_times to trials within a pprox based on recording time.
 
     trials: an iterable list of pproc objects, sorted in order of time. Each
     object must have a "recording" field that contains "start" and "stop"
@@ -21,34 +21,43 @@ def assign_events(pprox, events):
     of the trial.
 
     """
-    from dlab.pprox import trial_iterator
     from copy import deepcopy
 
+    # clear out the events; the default for oeaudio-trials is to store the click times
+    for trial in pprox["pprox"]:
+        trial["events"] = []
+    # trial iterator pulls sampling rate from entry metadata
+    def trial_iterator():
+        for i, trial in enumerate(pprox["pprox"]):
+            entry = trial["recording"]["entry"]
+            fs = pprox["entry_metadata"][entry]["sampling_rate"]
+            yield (i, trial, fs)
+
     clusters = {}
-    trial_iter = trial_iterator(pprox)
-    index, trial = next(trial_iter)
-    for channel, time, clust in events:
+    trial_iter = trial_iterator()
+    index, trial, sampling_rate = next(trial_iter)
+    for time, clust in zip(event_times, event_clusts):
         if time < trial["recording"]["start"]:
-            log.warning(
-                "warning: spike at %d is before the start of trial %d", time, index
-            )
+            if warn_unassigned:
+                log.warning(
+                    "warning: spike at %d is before the start of trial %d", time, index
+                )
             continue
         while time > trial["recording"]["stop"]:
             try:
-                index, trial = next(trial_iter)
+                index, trial, sampling_rate = next(trial_iter)
             except StopIteration:
-                log.warning(
-                    "warning: spike at %d is after the end of the last trial (%d samples)",
-                    time,
-                    trial["recording"]["stop"],
-                )
+                if warn_unassigned:
+                    log.warning(
+                        "warning: spike at %d is after the end of the last trial (%d samples)",
+                        time,
+                        trial["recording"]["stop"],
+                    )
                 break
-        t_seconds = (time - trial["recording"]["start"]) / trial["recording"][
-            "sampling_rate"
-        ]
+        t_seconds = (time - trial["recording"]["start"]) / sampling_rate
         if clust not in clusters:
             cluster = deepcopy(pprox)
-            cluster.update(cluster=clust, channel=channel)
+            cluster.update(cluster=clust)
             clusters[clust] = cluster
         clusters[clust]["pprox"][index]["events"].append(t_seconds)
     return clusters
@@ -106,7 +115,8 @@ def group_spikes_script(argv=None):
     groupfile = os.path.join(args.sortdir, "cluster_group.tsv")
     log.info("    - spike times: %s", timefile)
     log.info("    - spike clusters: %s", clustfile)
-    events = np.column_stack((np.load(timefile), np.load(clustfile)))
+    event_times = np.load(timefile).squeeze()
+    event_clusts = np.load(clustfile)
     log.info("    - cluster info: %s", infofile)
     info = pd.read_csv(infofile, sep="\t", index_col=0)
 
@@ -114,13 +124,23 @@ def group_spikes_script(argv=None):
         base, rec_id = nbank.parse_resource_id(pprox["recording"])
         args.name = rec_id
 
-    log.info("- grouping spikes by cluster and trial...")
-    clusters = assign_events(pprox, events)
+    log.info("- grouping %d spikes by cluster and trial...", event_times.size)
+    clusters = assign_events(pprox, event_times, event_clusts, warn_unassigned=args.debug)
+    total_spikes = 0
+    total_clusters = 0
     for clust_id, cluster in clusters.items():
+        clust_type = info.loc[clust_id]['group']
+        # annotate with cluster info
+        n_spikes = sum(len(t["events"]) for t in cluster["pprox"])
+        if clust_type == "noise" or (clust_type == "mua" and not args.mua):
+            log.info("  - cluster %d (%d spikes, %s) -> skipped", clust_id, n_spikes, clust_type)
+            continue
+        total_spikes += n_spikes
+        total_clusters += 1
         outfile = os.path.join(
             args.output or "", "{}_c{}.pprox".format(args.name, clust_id)
         )
-        log.info("  - cluster %d -> %s", clust_id, outfile)
+        log.info("  - cluster %d (%d spikes, %s) -> %s", clust_id, n_spikes, clust_type, outfile)
         try:
             pb = cluster["processed_by"]
         except KeyError:
@@ -129,3 +149,4 @@ def group_spikes_script(argv=None):
         pb.append("{} {}".format(p.prog, __version__))
         with open(outfile, "wt") as ofp:
             json.dump(cluster, ofp, default=json_serializable)
+    log.info("- a total of %d spikes were assigned to %d clusters", total_spikes, total_clusters)
