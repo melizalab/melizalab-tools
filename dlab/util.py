dca1ed5aae62b57fbf10ffd7217e7db6d91eb452
Dan Meliza <dan@meliza.org>
2021-04-19 21:24:18 -0400

    add memoize function

--- dlab/util.py
+++ dlab/util.py
@@ -64,3 +64,12 @@ def __js_numpy(val):
 def __js_numpy_arr(arr):
     """Used if *arr* is an instance of a numpy array."""
     return arr.tolist()
+
+
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return memodict().__getitem__
